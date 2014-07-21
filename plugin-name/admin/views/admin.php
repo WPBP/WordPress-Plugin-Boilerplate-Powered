8c91f577b94f3b4da0690cab3b3bb8e997c4275f
Mte90 <mte90smanettator@gmail.com>
2014-07-21 09:41:53 +0200

    added import/export settings

--- plugin-name/admin/views/admin.php
+++ plugin-name/admin/views/admin.php
@@ -23,18 +23,20 @@
 <div class="wrap">
 
 	<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
+
 	<div id="tabs">
 		<ul>
 			<li><a href="#tabs-1"><?php _e( 'Settings' ); ?></a></li>
 			<li><a href="#tabs-2"><?php _e( 'Settings 2', $this->plugin_slug ); ?></a></li>
+			<li><a href="#tabs-3"><?php _e( 'Import/Export', $this->plugin_slug ); ?></a></li>
 		</ul>
 		<div id="tabs-1">
 			<?php
 			// NOTE:Code for CMBF!
-
+print_r(get_option( $this->plugin_slug. '-settings' ));
 			$option_fields = array(
 				'id' => $this->plugin_slug . '_options',
-				'cmb_show_on' => array( 'key' => 'options-page', 'value' => array( $this->plugin_slug . '-settings', ), ),
+				'cmb_show_on' => array( 'key' => 'options-page', 'value' => array( $this->plugin_slug ), ),
 				'show_names' => true,
 				'fields' => array(
 					array(
@@ -61,32 +63,64 @@
 		<div id="tabs-2">
 			<?php
 			// NOTE:Code for CMBF!
-
-			$option_fields = array(
-				'id' => $this->plugin_slug . '_options',
-				'cmb_show_on' => array( 'key' => 'options-page', 'value' => array( $this->plugin_slug . '-settings', ), ),
+print_r(get_option( $this->plugin_slug. '-settings-second' ));
+			$option_fields_second = array(
+				'id' => $this->plugin_slug . '_options-second',
+				'cmb_show_on' => array( 'key' => 'options-page', 'value' => array( $this->plugin_slug ), ),
 				'show_names' => true,
 				'fields' => array(
 					array(
 						'name' => __( 'Text2', $this->plugin_slug ),
 						'desc' => __( 'field description (optional)', $this->plugin_slug ),
-						'id' => $this->plugin_slug . '_text2',
+						'id' => $this->plugin_slug . '_text-second',
 						'type' => 'text',
 					),
 					array(
 						'name' => __( 'Color Picker2', $this->plugin_slug ),
 						'desc' => __( 'field description (optional)', $this->plugin_slug ),
-						'id' => $this->plugin_slug . '_colorpicker2',
+						'id' => $this->plugin_slug . '_colorpicker-second',
 						'type' => 'colorpicker',
 						'default' => '#ffffff'
 					),
 				),
 			);
 
-			cmb_metabox_form( $option_fields, $this->plugin_slug . '-settings2' );
+			cmb_metabox_form( $option_fields_second, $this->plugin_slug . '-settings-second' );
 			?>
 
 			<!-- @TODO: Provide other markup for your options page here. -->
+		</div>
+		<div id="tabs-3">
+			<div class="postbox">
+				<h3><span><?php _e( 'Export Settings', $this->plugin_slug ); ?></span></h3>
+				<div class="inside">
+					<p><?php _e( 'Export the plugin settings for this site as a .json file. This allows you to easily import the configuration into another site.', $this->plugin_slug ); ?></p>
+					<form method="post">
+						<p><input type="hidden" name="pn_action" value="export_settings" /></p>
+						<p>
+							<?php wp_nonce_field( 'pn_export_nonce', 'pn_export_nonce' ); ?>
+							<?php submit_button( __( 'Export' ), 'secondary', 'submit', false ); ?>
+						</p>
+					</form>
+				</div>
+			</div>
+
+			<div class="postbox">
+				<h3><span><?php _e( 'Import Settings', $this->plugin_slug ); ?></span></h3>
+				<div class="inside">
+					<p><?php _e( 'Import the plugin settings from a .json file. This file can be obtained by exporting the settings on another site using the form above.', $this->plugin_slug ); ?></p>
+					<form method="post" enctype="multipart/form-data">
+						<p>
+							<input type="file" name="import_file"/>
+						</p>
+						<p>
+							<input type="hidden" name="pn_action" value="import_settings" />
+							<?php wp_nonce_field( 'pn_import_nonce', 'pn_import_nonce' ); ?>
+							<?php submit_button( __( 'Import' ), 'secondary', 'submit', false ); ?>
+						</p>
+					</form>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>

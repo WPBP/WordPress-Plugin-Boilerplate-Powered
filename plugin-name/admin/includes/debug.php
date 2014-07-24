64318f44ae58299e7e122276083b6039416f133c
Mte90 <mte90smanettator@gmail.com>
2014-07-25 01:38:58 +0200

    added debug system

---
+++ plugin-name/admin/includes/debug.php
@@ -0,0 +1,112 @@
+<?php
+
+/**
+ * Provides interface for debugging variables
+ *
+ * @author Benjamin J. Balter <ben@balter.com> & Mte90
+ */
+class Pn_Debug {
+
+	public $history = array();
+	private $parent;
+
+	/**
+	 * Register with WordPress API on construct
+	 * @param class $parent the parent class
+	 */
+	function __construct( &$parent ) {
+
+		$this->parent = &$parent;
+
+		add_action( 'init', array( $this, 'init' ), 5 );
+	}
+
+	/**
+	 * Check user cap and WP_DEBUG on init to see if class should continue loading
+	 */
+	function init() {
+
+		if ( !current_user_can( 'manage_options' ) || !WP_DEBUG ) {
+			return;
+		}
+
+		add_filter( 'debug_bar_panels', array( $this, 'init_panel' ) );
+		add_filter( 'debug_bar_panels', array( $this, 'register_panel' ), 20 );
+	}
+
+	/**
+	 * Debugs a variable
+	 * Only visible to admins if WP_DEBUG is on
+	 * @param mixed $var the var to debug
+	 * @param bool $die (optional) whether to die after outputting
+	 * @param string $function (optional) the function to call, usually either print_r or var_dump, but can be anything
+	 * @return unknown
+	 */
+	function log( $var, $die = false, $function = 'var_dump' ) {
+
+		if ( !current_user_can( 'manage_options' ) || !WP_DEBUG ) {
+			return;
+		}
+
+		ob_start();
+		echo "<!-- BEGIN DEBUG OUTPUT --><PRE>\n";
+		call_user_func( $function, $var );
+		echo "\n</PRE><!-- END DEBUG OUTPUT -->\n";
+
+		if ( $die ) {
+			die();
+		}
+
+		$debug = ob_get_clean();
+
+		$this->history[] = $debug;
+
+		//allow this to be used as a filter
+		return $var;
+	}
+
+	/**
+	 * Registers panel with debug bar
+	 * @param array $panels default panels
+	 * @return array the modified panels
+	 */
+	function register_panel( $panels ) {
+		$panels[] = new Pn_Debug_Panel( $this->parent->plugin_name . ' Debug', $this->history );
+		return $panels;
+	}
+
+	/**
+	 * Because you can't declare a class within a class, create an anonymous function to extend Debug_Bar_Panel
+	 * @param array $panels the default panels
+	 * @return array passback the original panels
+	 */
+	function init_panel( $panels ) {
+
+		$init = create_function( '', 'class Pn_Debug_Panel extends Debug_Bar_Panel {
+			protected $history;
+
+			function __construct( $name, &$instance ) {
+				$this->history = &$instance;
+				parent::__construct( $name );
+			}
+
+			function render() {
+				foreach ( $this->history as $debug ) {
+					echo "<pre>$debug</pre>";
+				}
+			}
+
+			function prerender() {
+				if ( empty( $this->history ) ) {
+					$this->set_visible( false );
+				}
+			}
+
+		}' );
+
+		$init();
+
+		return $panels;
+	}
+
+}

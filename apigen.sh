294307bd6ff9ac79d06735d85e5ec8a1f2f6e391
Mte90 <mte90smanettator@gmail.com>
2014-09-05 01:50:28 +0200

    add the icon the documentation

--- apigen.sh
+++ apigen.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e
 
+slug="plugin-name"
+
 if [ -d "./docs" ]; then
 	rm -r ./docs
 fi
 
-apigen --source plugin-name/ --exclude=*CMB*,*WP-Admin-Notice*,*WP-Contextual-Help*,*CPT_Core*,*Taxonomy_Core*,*Widget-Boilerplate* --destination docs --title "Wordpress Plugin Boilerplate Powered"
+apigen --source $slug/ --exclude=*CMB*,*WP-Admin-Notice*,*WP-Contextual-Help*,*CPT_Core*,*Taxonomy_Core*,*Widget-Boilerplate* --destination docs --title "Wordpress Plugin Boilerplate Powered"
+
+echo "#menu > span:first-child, #menu > a:first-child span {background: url('../../$slug/assets/icon-128x128.png');content: "";height: 128px;width: 128px;display:block;}" >> ./docs/resources/style.css
+echo "#menu > span:first-child, #menu > a:first-child, #menu > a:first-child:hover {background-color:transparent;color: transparent;}" >> ./docs/resources/style.css

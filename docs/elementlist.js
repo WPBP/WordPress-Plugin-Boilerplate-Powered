ab95c762a58d668f3fe7b8c198ae2f7bb5253cfc
Mte90 <mte90smanettator@gmail.com>
2014-09-05 11:16:39 +0200

    remvoed doc

--- docs/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["f","deregister_string()"],["c","Exception"],["c","Fake_Page"],["f","get_language()"],["f","get_string()"],["c","PHP_Extension_Requirement"],["c","PHP_Requirement"],["c","Plugin_Name"],["c","Plugin_Name_Admin"],["c","Plugin_Requirement"],["c","Plugin_Requirements"],["c","Plugin_Requirements_Exception"],["c","Pn_Debug"],["f","pn_get_template_part()"],["f","register_string()"],["c","Requirement_Error"],["c","WordPress_Requirement"]];

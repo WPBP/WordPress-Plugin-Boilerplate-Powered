a28c92685f70f56ae14bd138418a55b591c20b0c
Mte90 <mte90smanettator@gmail.com>
2014-10-31 13:01:49 +0100

    mispelling

--- plugin-name/plugin-name.php
+++ plugin-name/plugin-name.php
@@ -23,7 +23,7 @@
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Domain Path:       /languages
- * WordPress-Plugin-Boilerplate-Powered: v1.0.0
+ * WordPress-Plugin-Boilerplate-Powered: v1.1.0
  */
 // If this file is called directly, abort.
 if ( !defined( 'WPINC' ) ) {

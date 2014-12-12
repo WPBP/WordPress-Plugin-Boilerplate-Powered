8ded1e895ed362550d24df499416488cc0be4b0e
Mte90 <mte90smanettator@gmail.com>
2014-12-13 00:16:25 +0100

    working on sass integration

---
+++ plugin-name/Gruntfile.js
@@ -0,0 +1,68 @@
+'use strict';
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    compass: {
+      admin: {
+        options: {
+          sassDir: 'admin/assets/sass',
+          cssDir: 'admin/assets/css',
+          environment: 'production',
+          relativeAssets: true
+        }
+      },
+      public: {
+        options: {
+          sassDir: 'public/assets/sass',
+          cssDir: 'public/assets/css',
+          environment: 'production',
+          relativeAssets: true
+        }
+      },
+      adminDev: {
+        options: {
+          environment: 'development',
+          debugInfo: true,
+          noLineComments: false,
+          sassDir: 'public/assets/sass',
+          cssDir: 'public/assets/css',
+          outputStyle: 'expanded',
+          relativeAssets: true
+        }
+      },
+      publicDev: {
+        options: {
+          environment: 'development',
+          debugInfo: true,
+          noLineComments: false,
+          sassDir: 'public/assets/sass',
+          cssDir: 'public/assets/css',
+          outputStyle: 'expanded',
+          relativeAssets: true
+        }
+      }
+    },
+    watch: {
+      compass: {
+        files: [
+          'admin/assets/sass/*.sass',
+          'public/assets/sass/*.sass'
+        ],
+        tasks: ['compass:adminDev', 'compass:publicDev']
+      }
+    }
+  });
+
+  // Load tasks
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+
+  // Register tasks
+  grunt.registerTask('default', [
+    'compass:admin', 'compass:public'
+  ]);
+  grunt.registerTask('dev', [
+    'watch'
+  ]);
+
+};

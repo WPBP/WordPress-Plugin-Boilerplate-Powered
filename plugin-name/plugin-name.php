0c49b299135bece48ad87b11119890a7791ae67c
Mte90 <mte90smanettator@gmail.com>
2015-07-13 14:55:45 +0200

    working on #26

--- plugin-name/plugin-name.php
+++ plugin-name/plugin-name.php
@@ -25,49 +25,56 @@
  * Domain Path:       /languages
  * WordPress-Plugin-Boilerplate-Powered: v1.1.2
  */
+
 // If this file is called directly, abort.
 if ( !defined( 'WPINC' ) ) {
 	die;
 }
 
-/* ----------------------------------------------------------------------------*
+/*
+ * ------------------------------------------------------------------------------
  * Public-Facing Functionality
- * ---------------------------------------------------------------------------- */
+ * ------------------------------------------------------------------------------
+ */
 
 /*
  * Load library for simple and fast creation of Taxonomy and Custom Post Type
- *
  */
+
 require_once( plugin_dir_path( __FILE__ ) . 'includes/Taxonomy_Core/Taxonomy_Core.php' );
 require_once( plugin_dir_path( __FILE__ ) . 'includes/CPT_Core/CPT_Core.php' );
 
 /*
  * Load template system
  */
+
 require_once( plugin_dir_path( __FILE__ ) . 'includes/template.php' );
 
 /*
  * Load Widgets Helper
  */
+
 require_once( plugin_dir_path( __FILE__ ) . 'includes/Widgets-Helper/wph-widget-class.php' );
 require_once( plugin_dir_path( __FILE__ ) . 'includes/widgets/sample.php' );
 
 /*
  * Load Fake Page class
  */
+
 require_once( plugin_dir_path( __FILE__ ) . 'includes/fake-page.php' );
 
 new Fake_Page(
-		array(
-	'slug' => 'fake_slug',
-	'post_title' => 'Fake Page Title',
-	'post content' => 'This is the fake page content'
-		)
+	array(
+    'slug' => 'fake_slug',
+    'post_title' => 'Fake Page Title',
+    'post content' => 'This is the fake page content'
+	)
 );
 
 /*
  * Load Language wrapper function for WPML/Ceceppa Multilingua/Polylang
  */
+
 require_once( plugin_dir_path( __FILE__ ) . 'includes/language.php' );
 
 /*
@@ -76,6 +83,7 @@ require_once( plugin_dir_path( __FILE__ ) . 'includes/language.php' );
  * - replace `class-plugin-name.php` with the name of the plugin's class file
  *
  */
+
 require_once( plugin_dir_path( __FILE__ ) . 'public/class-plugin-name.php' );
 
 /*
@@ -87,6 +95,7 @@ require_once( plugin_dir_path( __FILE__ ) . 'public/class-plugin-name.php' );
  * - replace Plugin_Name with the name of the class defined in
  *   `class-plugin-name.php`
  */
+
 register_activation_hook( __FILE__, array( 'Plugin_Name', 'activate' ) );
 register_deactivation_hook( __FILE__, array( 'Plugin_Name', 'deactivate' ) );
 
@@ -95,12 +104,18 @@ register_deactivation_hook( __FILE__, array( 'Plugin_Name', 'deactivate' ) );
  *
  * - replace Plugin_Name with the name of the class defined in
  *   `class-plugin-name.php`
+ * - 9999 is used for load the plugin as last for resolve some
+ *   problems when the plugin use API of other plugins, remove
+ *   if you don' want this
  */
-add_action( 'plugins_loaded', array( 'Plugin_Name', 'get_instance' ) );
 
-/* ----------------------------------------------------------------------------*
+add_action( 'plugins_loaded', array( 'Plugin_Name', 'get_instance' ), 9999 );
+
+/*
+ * -----------------------------------------------------------------------------
  * Dashboard and Administrative Functionality
- * ---------------------------------------------------------------------------- */
+ * -----------------------------------------------------------------------------
+*/
 
 /*
  * @TODO:
@@ -118,6 +133,7 @@ add_action( 'plugins_loaded', array( 'Plugin_Name', 'get_instance' ) );
  *
  * The code below is intended to to give the lightest footprint possible.
  */
+
 if ( is_admin() && (!defined( 'DOING_AJAX' ) || !DOING_AJAX ) ) {
 
 	require_once( plugin_dir_path( __FILE__ ) . 'admin/class-plugin-name-admin.php' );

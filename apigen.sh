d1f734da0cb440daaf497c4d024bc523066b70eb
Daniele Scasciafratte <mte90net@gmail.com>
2015-07-25 14:50:02 +0200

    update apigen syntax

--- apigen.sh
+++ apigen.sh
@@ -7,7 +7,7 @@ if [ -d "./docs" ]; then
 	rm -r ./docs
 fi
 
-apigen --source $slug/ --exclude=*CMB*,*WP-Admin-Notice*,*WP-Contextual-Help*,*CPT_Core*,*Taxonomy_Core*,*Widget-Boilerplate* --destination docs --title "Wordpress Plugin Boilerplate Powered"
+apigen --source=$slug/ --exclude=*CMB*,*WP-Admin-Notice*,*WP-Contextual-Help*,*CPT_Core*,*Taxonomy_Core* --destination=docs --title="Wordpress Plugin Boilerplate Powered"
 
 echo "#menu > span:first-child, #menu > a:first-child span {background: url('../../$slug/assets/icon-128x128.png');content: "";height: 128px;width: 128px;display:block;}" >> ./docs/resources/style.css
 echo "#menu > span:first-child, #menu > a:first-child, #menu > a:first-child:hover {background-color:transparent;color: transparent;}" >> ./docs/resources/style.css

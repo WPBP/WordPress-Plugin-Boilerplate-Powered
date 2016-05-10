707b3bf661bbaf78f2de7d3dc14b2f9a8325d896
Mattia Migliorini <deshack@ubuntu.com>
2016-05-10 11:58:01 +0200

    Indentation-independent version replacement

--- wp-boilerplate-version.sh
+++ wp-boilerplate-version.sh
@@ -16,8 +16,7 @@ slug=`basename $(readlink -f $1)`
 LINE=`sed -n '/Stable tag: /{=}' README.txt`
 sed -i "${LINE}s/.*/Stable tag: ${2}/" README.txt
 
-LINE=`sed -n '/    const VERSION = /{=}' public/class-${slug}.php`
-sed -i "${LINE}s/.*/    const VERSION = '${2}';/" public/class-${slug}.php
+sed -i "s/const VERSION = '.*';/const VERSION = '${2}';/" public/class-${slug}.php
 
 LINE=`sed -n '/ * Version:/{=}' ${slug}.php`
 sed -i "${LINE}s/.*/ * Version:           ${2}/" ${slug}.php

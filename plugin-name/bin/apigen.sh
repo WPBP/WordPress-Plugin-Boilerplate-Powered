4ac57f55ef4ce5b0034c7903ed472b21d15d0c59
Daniele Scasciafratte <mte90net@gmail.com>
2016-07-08 14:41:30 +0200

    fix #58

--- plugin-name/bin/apigen.sh
+++ plugin-name/bin/apigen.sh
@@ -4,10 +4,10 @@ set -e
 slug="plugin-name"
 
 if [ -d "./docs" ]; then
-	rm -r ./docs
+	rm -r ../docs
 fi
 
-apigen generate --source=$slug/ --exclude=*CMB*,*WP-Admin-Notice*,*WP-Contextual-Help*,*CPT_Core*,*Taxonomy_Core*,*wp-background-processing*,*freemius*,*Plus*,*WP-Contextual-Help*,*WP-Admin-Notice* --destination=docs --title="Wordpress Plugin Boilerplate Powered"
+apigen generate --source=$slug/ --exclude=public/includes/lib,admin/includes/lib,includes/lib,/vendor --destination=docs --title="Wordpress Plugin Boilerplate Powered"
 
 echo "#menu > span:first-child, #menu > a:first-child span {background: url('../../$slug/assets/icon-256x256.png');content: '';height: 256px;width: 240px;display:block;margin: 0 auto;}" >> ./docs/resources/style.css
 echo "#menu > span:first-child, #menu > a:first-child, #menu > a:first-child:hover {background-color:transparent;color: transparent;}" >> ./docs/resources/style.css

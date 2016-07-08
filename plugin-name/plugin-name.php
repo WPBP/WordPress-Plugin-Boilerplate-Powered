cf44f8412d9e3aa369f21e3e323e36baf677eb71
Daniele Scasciafratte <mte90net@gmail.com>
2016-07-08 20:20:00 +0200

    fix #14

--- plugin-name/plugin-name.php
+++ plugin-name/plugin-name.php
@@ -24,64 +24,70 @@
  * Domain Path:       /languages
  * WordPress-Plugin-Boilerplate-Powered: v2.0.0
  */
-
 // If this file is called directly, abort.
 if ( !defined( 'WPINC' ) ) {
-	die;
+  die;
 }
 
-define('PN_VERSION', '1.0.0');
-define('PN_TEXTDOMAIN', 'plugin-name');
-define('PN_NAME', 'Plugin Name');
+define( 'PN_VERSION', '1.0.0' );
+define( 'PN_TEXTDOMAIN', 'plugin-name' );
+define( 'PN_NAME', 'Plugin Name' );
 
 /*
  * ------------------------------------------------------------------------------
  * Public-Facing Functionality
  * ------------------------------------------------------------------------------
  */
+
 function pn_load_plugin_textdomain() {
-	load_plugin_textdomain( PN_TEXTDOMAIN, FALSE, basename( plugin_dir_path( dirname( __FILE__ ) ) ) . '/languages/' );
+  load_plugin_textdomain( PN_TEXTDOMAIN, FALSE, basename( plugin_dir_path( dirname( __FILE__ ) ) ) . '/languages/' );
 }
+
 add_action( 'plugins_loaded', 'pn_load_plugin_textdomain', 1 );
 
 require_once( plugin_dir_path( __FILE__ ) . 'composer/autoload.php' );
 
-/** 
+/*
+ * Posts 2 Posts integration
+ */
+require_once( plugin_dir_path( __FILE__ ) . 'includes/PN_P2P.php' );
+
+/**
  * Create a helper function for easy SDK access.
  * 
  * @global type $pn_fs
  * @return object
  */
 function pn_fs() {
-    global $pn_fs;
+  global $pn_fs;
 
-    if ( ! isset( $pn_fs ) ) {
-        $pn_fs = fs_dynamic_init( array(
-            'id'                => '',
-            'slug'              => 'plugin-name',
-            'public_key'        => '',
-            'is_live'           => false,
-            'is_premium'        => true,
-            'has_addons'        => false,
-            'has_paid_plans'    => true,
-            'menu'              => array(
-                'slug'       => 'plugin-name'
-            ),
-        ) );
-    }
+  if ( !isset( $pn_fs ) ) {
+    $pn_fs = fs_dynamic_init( array(
+	  'id' => '',
+	  'slug' => 'plugin-name',
+	  'public_key' => '',
+	  'is_live' => false,
+	  'is_premium' => true,
+	  'has_addons' => false,
+	  'has_paid_plans' => true,
+	  'menu' => array(
+		'slug' => 'plugin-name'
+	  ),
+		) );
+  }
 
-    return $pn_fs;
+  return $pn_fs;
 }
 
 // Init Freemius.
 // pn_fs();
 
 new Fake_Page(
-	array(
+	  array(
     'slug' => 'fake_slug',
     'post_title' => 'Fake Page Title',
     'post_content' => 'This is the fake page content'
-	)
+	  )
 );
 
 require_once( plugin_dir_path( __FILE__ ) . 'public/Plugin_Name.php' );
@@ -91,7 +97,7 @@ require_once( plugin_dir_path( __FILE__ ) . 'includes/PN_ActDeact.php' );
  * -----------------------------------------------------------------------------
  * Dashboard and Administrative Functionality
  * -----------------------------------------------------------------------------
-*/
+ */
 
 /*
  * If you want to include Ajax within the dashboard, change the following
@@ -105,5 +111,5 @@ require_once( plugin_dir_path( __FILE__ ) . 'includes/PN_ActDeact.php' );
  */
 
 if ( is_admin() && (!defined( 'DOING_AJAX' ) || !DOING_AJAX ) ) {
-	require_once( plugin_dir_path( __FILE__ ) . 'admin/Plugin_Name_Admin.php' );
+  require_once( plugin_dir_path( __FILE__ ) . 'admin/Plugin_Name_Admin.php' );
 }

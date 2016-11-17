1da6b5f5c258aa11a539059d5be7a657ed52beb3
Daniele Scasciafratte <mte90net@gmail.com>
2016-11-17 18:51:59 +0100

    improving the admin view code

--- plugin-name/admin/views/admin.php
+++ plugin-name/admin/views/admin.php
@@ -41,101 +41,101 @@
 		$cmb->add_field( array(
 		    'name' => __( 'Text', PN_TEXTDOMAIN ),
 		    'desc' => __( 'field description (optional)', PN_TEXTDOMAIN ),
-		    'id' => PN_TEXTDOMAIN . '_text',
+		    'id' => 'text',
 		    'type' => 'text',
 		    'default' => 'Default Text',
 		) );
 		$cmb->add_field( array(
 		    'name' => __( 'Color Picker', PN_TEXTDOMAIN ),
 		    'desc' => __( 'field description (optional)', PN_TEXTDOMAIN ),
-		    'id' => PN_TEXTDOMAIN . '_colorpicker',
+		    'id' => 'colorpicker',
 		    'type' => 'colorpicker',
 		    'default' => '#bada55',
 		) );
 		$cmb->add_field( array(
 		    'name' => __( 'Test Text Medium', PN_TEXTDOMAIN ),
 		    'desc' => __( 'field description (optional)', PN_TEXTDOMAIN ),
-		    'id' => PN_TEXTDOMAIN . '_textmedium',
+		    'id' => '_textmedium',
 		    'type' => 'text_medium',
 			  // 'repeatable' => true,
 		) );
 		$cmb->add_field( array(
 		    'name' => __( 'Website URL', PN_TEXTDOMAIN ),
 		    'desc' => __( 'field description (optional)', PN_TEXTDOMAIN ),
-		    'id' => PN_TEXTDOMAIN . '_url',
+		    'id' => '_url',
 		    'type' => 'text_url',
 		) );
 		$cmb->add_field( array(
 		    'name' => __( 'Test Text Email', PN_TEXTDOMAIN ),
 		    'desc' => __( 'field description (optional)', PN_TEXTDOMAIN ),
-		    'id' => PN_TEXTDOMAIN . '_email',
+		    'id' => '_email',
 		    'type' => 'text_email',
 		) );
 		$cmb->add_field( array(
 		    'name' => __( 'Test Time', PN_TEXTDOMAIN ),
 		    'desc' => __( 'field description (optional)', PN_TEXTDOMAIN ),
-		    'id' => PN_TEXTDOMAIN . '_time',
+		    'id' => '_time',
 		    'type' => 'text_time',
 		) );
 		$cmb->add_field( array(
 		    'name' => __( 'Time zone', PN_TEXTDOMAIN ),
 		    'desc' => __( 'Time zone', PN_TEXTDOMAIN ),
-		    'id' => PN_TEXTDOMAIN . '_timezone',
+		    'id' => '_timezone',
 		    'type' => 'select_timezone',
 		) );
 		$cmb->add_field( array(
 		    'name' => __( 'Test Date Picker', PN_TEXTDOMAIN ),
 		    'desc' => __( 'field description (optional)', PN_TEXTDOMAIN ),
-		    'id' => PN_TEXTDOMAIN . '_textdate',
+		    'id' => '_textdate',
 		    'type' => 'text_date',
 		) );
 		$cmb->add_field( array(
 		    'name' => __( 'Test Date Picker (UNIX timestamp)', PN_TEXTDOMAIN ),
 		    'desc' => __( 'field description (optional)', PN_TEXTDOMAIN ),
-		    'id' => PN_TEXTDOMAIN . '_textdate_timestamp',
+		    'id' => '_textdate_timestamp',
 		    'type' => 'text_date_timestamp',
 		) );
 		$cmb->add_field( array(
 		    'name' => __( 'Test Date/Time Picker Combo (UNIX timestamp)', PN_TEXTDOMAIN ),
 		    'desc' => __( 'field description (optional)', PN_TEXTDOMAIN ),
-		    'id' => PN_TEXTDOMAIN . '_datetime_timestamp',
+		    'id' => '_datetime_timestamp',
 		    'type' => 'text_datetime_timestamp',
 		) );
 		$cmb->add_field( array(
 		    'name' => __( 'Test Money', PN_TEXTDOMAIN ),
 		    'desc' => __( 'field description (optional)', PN_TEXTDOMAIN ),
-		    'id' => PN_TEXTDOMAIN . '_textmoney',
+		    'id' => '_textmoney',
 		    'type' => 'text_money',
 		    'before_field' => '€', // Override '$' symbol if needed
 		) );
 		$cmb->add_field( array(
 		    'name' => __( 'Test Text Area', PN_TEXTDOMAIN ),
 		    'desc' => __( 'field description (optional)', PN_TEXTDOMAIN ),
-		    'id' => PN_TEXTDOMAIN . '_textarea',
+		    'id' => '_textarea',
 		    'type' => 'textarea',
 		) );
 		$cmb->add_field( array(
 		    'name' => __( 'Test Text Area Small', PN_TEXTDOMAIN ),
 		    'desc' => __( 'field description (optional)', PN_TEXTDOMAIN ),
-		    'id' => PN_TEXTDOMAIN . '_textareasmall',
+		    'id' => '_textareasmall',
 		    'type' => 'textarea_small',
 		) );
 		$cmb->add_field( array(
 		    'name' => __( 'Test Text Area for Code', PN_TEXTDOMAIN ),
 		    'desc' => __( 'field description (optional)', PN_TEXTDOMAIN ),
-		    'id' => PN_TEXTDOMAIN . '_textarea_code',
+		    'id' => '_textarea_code',
 		    'type' => 'textarea_code',
 		) );
 		$cmb->add_field( array(
 		    'name' => __( 'Test Title Weeeee', PN_TEXTDOMAIN ),
 		    'desc' => __( 'This is a title description', PN_TEXTDOMAIN ),
-		    'id' => PN_TEXTDOMAIN . '_title',
+		    'id' => '_title',
 		    'type' => 'title',
 		) );
 		$cmb->add_field( array(
 		    'name' => __( 'Test Select', PN_TEXTDOMAIN ),
 		    'desc' => __( 'field description (optional)', PN_TEXTDOMAIN ),
-		    'id' => PN_TEXTDOMAIN . '_select',
+		    'id' => '_select',
 		    'type' => 'select',
 		    'show_option_none' => true,
 		    'options' => array(
@@ -147,7 +147,7 @@
 		$cmb->add_field( array(
 		    'name' => __( 'Test Radio inline', PN_TEXTDOMAIN ),
 		    'desc' => __( 'field description (optional)', PN_TEXTDOMAIN ),
-		    'id' => PN_TEXTDOMAIN . '_radio_inline',
+		    'id' => '_radio_inline',
 		    'type' => 'radio_inline',
 		    'show_option_none' => 'No Selection',
 		    'options' => array(
@@ -159,7 +159,7 @@
 		$cmb->add_field( array(
 		    'name' => __( 'Test Radio', PN_TEXTDOMAIN ),
 		    'desc' => __( 'field description (optional)', PN_TEXTDOMAIN ),
-		    'id' => PN_TEXTDOMAIN . '_radio',
+		    'id' => '_radio',
 		    'type' => 'radio',
 		    'options' => array(
 			  'option1' => __( 'Option One', PN_TEXTDOMAIN ),
@@ -170,7 +170,7 @@
 		$cmb->add_field( array(
 		    'name' => __( 'Test Taxonomy Radio', PN_TEXTDOMAIN ),
 		    'desc' => __( 'field description (optional)', PN_TEXTDOMAIN ),
-		    'id' => PN_TEXTDOMAIN . '_text_taxonomy_radio',
+		    'id' => '_text_taxonomy_radio',
 		    'type' => 'taxonomy_radio',
 		    'taxonomy' => 'category', // Taxonomy Slug
 			  // 'inline'  => true, // Toggles display to inline
@@ -178,27 +178,27 @@
 		$cmb->add_field( array(
 		    'name' => __( 'Test Taxonomy Select', PN_TEXTDOMAIN ),
 		    'desc' => __( 'field description (optional)', PN_TEXTDOMAIN ),
-		    'id' => PN_TEXTDOMAIN . '_taxonomy_select',
+		    'id' => '_taxonomy_select',
 		    'type' => 'taxonomy_select',
 		    'taxonomy' => 'category', // Taxonomy Slug
 		) );
 		$cmb->add_field( array(
 		    'name' => __( 'Test Taxonomy Multi Checkbox', PN_TEXTDOMAIN ),
 		    'desc' => __( 'field description (optional)', PN_TEXTDOMAIN ),
-		    'id' => PN_TEXTDOMAIN . '_multitaxonomy',
+		    'id' => '_multitaxonomy',
 		    'type' => 'taxonomy_multicheck',
 		    'taxonomy' => 'category', // Taxonomy Slug
 		) );
 		$cmb->add_field( array(
 		    'name' => __( 'Test Checkbox', PN_TEXTDOMAIN ),
 		    'desc' => __( 'field description (optional)', PN_TEXTDOMAIN ),
-		    'id' => PN_TEXTDOMAIN . '_checkbox',
+		    'id' => '_checkbox',
 		    'type' => 'checkbox',
 		) );
 		$cmb->add_field( array(
 		    'name' => __( 'Test Multi Checkbox', PN_TEXTDOMAIN ),
 		    'desc' => __( 'field description (optional)', PN_TEXTDOMAIN ),
-		    'id' => PN_TEXTDOMAIN . '_multicheckbox',
+		    'id' => '_multicheckbox',
 		    'type' => 'multicheck',
 		    'options' => array(
 			  'check1' => __( 'Check One', PN_TEXTDOMAIN ),
@@ -209,32 +209,32 @@
 		$cmb->add_field( array(
 		    'name' => __( 'Test wysiwyg', PN_TEXTDOMAIN ),
 		    'desc' => __( 'field description (optional)', PN_TEXTDOMAIN ),
-		    'id' => PN_TEXTDOMAIN . '_wysiwyg',
+		    'id' => '_wysiwyg',
 		    'type' => 'wysiwyg',
 		    'options' => array( 'textarea_rows' => 5, ),
 		) );
 		$cmb->add_field( array(
 		    'name' => __( 'Test Image', PN_TEXTDOMAIN ),
 		    'desc' => __( 'Upload an image or enter a URL.', PN_TEXTDOMAIN ),
-		    'id' => PN_TEXTDOMAIN . '_image',
+		    'id' => '_image',
 		    'type' => 'file',
 		) );
 		$cmb->add_field( array(
 		    'name' => __( 'Multiple Files', PN_TEXTDOMAIN ),
 		    'desc' => __( 'Upload or add multiple images/attachments.', PN_TEXTDOMAIN ),
-		    'id' => PN_TEXTDOMAIN . '_file_list',
+		    'id' => '_file_list',
 		    'type' => 'file_list',
 		    'preview_size' => array( 100, 100 ), // Default: array( 50, 50 )
 		) );
 		$cmb->add_field( array(
 		    'name' => __( 'oEmbed', PN_TEXTDOMAIN ),
 		    'desc' => __( 'Enter a youtube, twitter, or instagram URL. Supports services listed at <a href="http://codex.wordpress.org/Embeds">http://codex.wordpress.org/Embeds</a>.', PN_TEXTDOMAIN ),
-		    'id' => PN_TEXTDOMAIN . '_embed',
+		    'id' => '_embed',
 		    'type' => 'oembed',
 		) );
 		$cmb->add_field( array(
 		    'name' => 'Testing Field Parameters',
-		    'id' => PN_TEXTDOMAIN . '_parameters',
+		    'id' => '_parameters',
 		    'type' => 'text',
 		    'before_row' => '<p>before_row_if_2</p>', // Callback
 		    'before' => '<p>Testing <b>"before"</b> parameter</p>',
@@ -263,14 +263,14 @@
 		$cmb->add_field( array(
 		    'name' => __( 'Text', PN_TEXTDOMAIN ),
 		    'desc' => __( 'field description (optional)', PN_TEXTDOMAIN ),
-		    'id' => PN_TEXTDOMAIN . '_text-second',
+		    'id' => '_text-second',
 		    'type' => 'text',
 		    'default' => 'Default Text',
 		) );
 		$cmb->add_field( array(
 		    'name' => __( 'Color Picker', PN_TEXTDOMAIN ),
 		    'desc' => __( 'field description (optional)', PN_TEXTDOMAIN ),
-		    'id' => PN_TEXTDOMAIN . '_colorpicker-second',
+		    'id' => '_colorpicker-second',
 		    'type' => 'colorpicker',
 		    'default' => '#bada55',
 		) );

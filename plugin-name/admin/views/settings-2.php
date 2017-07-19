83d1add8c7736364e4c4e86fe41e58bb3463d37a
Daniele Scasciafratte <mte90net@gmail.com>
2017-07-19 19:28:07 +0200

    fix #80

---
+++ plugin-name/admin/views/settings-2.php
@@ -0,0 +1,31 @@
+
+		<div id="tabs-2" class="wrap">
+			<?php
+			//WPBPGen{{#unless libraries_webdevstudios__cmb2}}
+			$cmb = new_cmb2_box( array(
+				'id' => PN_TEXTDOMAIN . '_options-second',
+				'hookup' => false,
+				'show_on' => array( 'key' => 'options-page', 'value' => array( PN_TEXTDOMAIN ), ),
+				'show_names' => true,
+					) );
+			$cmb->add_field( array(
+				'name' => __( 'Text', PN_TEXTDOMAIN ),
+				'desc' => __( 'field description (optional)', PN_TEXTDOMAIN ),
+				'id' => '_text-second',
+				'type' => 'text',
+				'default' => 'Default Text',
+			) );
+			$cmb->add_field( array(
+				'name' => __( 'Color Picker', PN_TEXTDOMAIN ),
+				'desc' => __( 'field description (optional)', PN_TEXTDOMAIN ),
+				'id' => '_colorpicker-second',
+				'type' => 'colorpicker',
+				'default' => '#bada55',
+			) );
+
+			cmb2_metabox_form( PN_TEXTDOMAIN . '_options-second', PN_TEXTDOMAIN . '-settings-second' );
+			//{{/unless}}
+			?>
+
+			<!-- @TODO: Provide other markup for your options page here. -->
+		</div>

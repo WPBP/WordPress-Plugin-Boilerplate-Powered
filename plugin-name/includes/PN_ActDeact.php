2670a12bc531d5183a6794d2022aeda770e25b1e
Daniele Scasciafratte <mte90net@gmail.com>
2017-07-19 22:23:46 +0200

    fix #84

--- plugin-name/includes/PN_ActDeact.php
+++ plugin-name/includes/PN_ActDeact.php
@@ -1,181 +1,164 @@
 <?php
 
 /**
- * This class contain the activate and deactive method and relates.
- *
+ * Plugin_Name
+ * 
  * @package   Plugin_Name
  * @author    {{author_name}} <{{author_email}}>
  * @copyright {{author_copyright}}
  * @license   {{author_license}}
  * @link      {{author_url}}
  */
+
+/**
+ * This class contain the activate and deactive method and relates.
+ */
 class Pn_ActDeact {
 
-  /**
-   * Initialize the Act/Deact
-   */
-  function __construct() {
-    $plugin = Plugin_Name::get_instance();
-    $this->plugin_roles = $plugin->get_plugin_roles();
-    // Activate plugin when new blog is added
-    add_action( 'wpmu_new_blog', array( $this, 'activate_new_site' ) );
+	/**
+	 * Initialize the Act/Deact
+	 * 
+	 * @return void
+	 */
+	function __construct() {
+		$plugin = Plugin_Name::get_instance();
+		$this->plugin_roles = $plugin->get_plugin_roles();
+		// Activate plugin when new blog is added
+		add_action( 'wpmu_new_blog', array( $this, 'activate_new_site' ) );
 
-    register_activation_hook( __FILE__, array( $this, 'activate' ) );
-    register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
-  }
-
-  /**
-   * Fired when the plugin is activated.
-   *
-   * @since {{plugin_version}}
-   *
-   * @param boolean $network_wide True if active in a multiste, false if classic site.
-   * 
-   * @return void
-   */
-  public static function activate( $network_wide ) {
-    if ( function_exists( 'is_multisite' ) && is_multisite() ) {
-	if ( $network_wide ) {
-	  // Get all blog ids
-	  $blog_ids = self::get_blog_ids();
-	  foreach ( $blog_ids as $blog_id ) {
-	    switch_to_blog( $blog_id );
-	    self::single_activate();
-	    restore_current_blog();
-	  }
-	} else {
-	  self::single_activate();
+		register_activation_hook( __FILE__, array( $this, 'activate' ) );
+		register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
 	}
-    } else {
-	self::single_activate();
-    }
-  }
 
-  /**
-   * Fired when the plugin is deactivated.
-   *
-   * @since {{plugin_version}}
-   *
-   * @param boolean $network_wide True if WPMU superadmin uses
-   *                              "Network Deactivate" action, false if
-   *                              WPMU is disabled or plugin is
-   *                              deactivated on an individual blog.
-   * 
-   * @return void
-   */
-  public static function deactivate( $network_wide ) {
-    if ( function_exists( 'is_multisite' ) && is_multisite() ) {
-	if ( $network_wide ) {
-	  // Get all blog ids
-	  $blog_ids = self::get_blog_ids();
-	  foreach ( $blog_ids as $blog_id ) {
-	    switch_to_blog( $blog_id );
-	    self::single_deactivate();
-	    restore_current_blog();
-	  }
-	} else {
-	  self::single_deactivate();
+	/**
+	 * Fired when the plugin is activated.
+	 *
+	 * @param boolean $network_wide True if active in a multiste, false if classic site.
+	 *
+	 * @since {{plugin_version}}
+	 * 
+	 * @return void
+	 */
+	public static function activate( $network_wide ) {
+		if ( function_exists( 'is_multisite' ) && is_multisite() ) {
+			if ( $network_wide ) {
+				// Get all blog ids
+				$blogs = get_sites();
+				foreach ( $blogs as $blog ) {
+					switch_to_blog( $blog->blog_id );
+					self::single_activate();
+					restore_current_blog();
+				}
+				return;
+			}
+		}
+		self::single_activate();
 	}
-    } else {
-	self::single_deactivate();
-    }
-  }
 
-  /**
-   * Fired when a new site is activated with a WPMU environment.
-   *
-   * @since {{plugin_version}}
-   *
-   * @param integer $blog_id ID of the new blog.
-   * 
-   * @return void
-   */
-  public function activate_new_site( $blog_id ) {
-    if ( 1 !== did_action( 'wpmu_new_blog' ) ) {
-	return;
-    }
-
-    switch_to_blog( $blog_id );
-    self::single_activate();
-    restore_current_blog();
-  }
-
-  /**
-   * Get all blog ids of blogs in the current network that are:
-   * - not archived
-   * - not spam
-   * - not deleted
-   *
-   * @since {{plugin_version}}
-   *
-   * @return array|false The blog ids, false if no matches.
-   */
-  private static function get_blog_ids() {
-    global $wpdb;
-
-    // Get an array of blog ids
-    $sql = 'SELECT blog_id FROM ' . $wpdb->blogs .
-		" WHERE archived = '0' AND spam = '0'
-			AND deleted = '0'";
-
-    return $wpdb->get_col( $sql );
-  }
-
-  /**
-   * Fired for each blog when the plugin is activated.
-   *
-   * @since {{plugin_version}}
-   * @return void
-   */
-  private static function single_activate() {
-    $plugin = Plugin_Name::get_instance();
-    $plugin_slug = $plugin->get_plugin_slug();
-    //WPBPGen{{#unless system_capability-system}}
-    $plugin_roles = $plugin->get_plugin_roles();
-    //{{/unless}}
-    //WPBPGen{{#unless libraries_wpbp__requirements}}
-    // Requirements Detection System - read the doc/example in the library file
-    require_once( plugin_dir_path( __FILE__ ) . 'requirements.php' );
-    new Plugin_Requirements( PN_NAME, $plugin_slug, array(
-	  'WP' => new WordPress_Requirement( '4.1.0' )
-		) );
-    //{{/unless}}
-    // @TODO: Define activation functionality here
-    //WPBPGen{{#unless system_capability-system}}
-    // add_role( 'advanced', __( 'Advanced' ) ); //Add a custom roles
-    global $wp_roles;
-    if ( !isset( $wp_roles ) ) {
-	$wp_roles = new WP_Roles;
-    }
-
-    foreach ( $wp_roles->role_names as $role => $label ) {
-	// If the role is a standard role, map the default caps, otherwise, map as a subscriber
-	$caps = ( array_key_exists( $role, $plugin_roles ) ) ? $plugin_roles[ $role ] : $plugin_roles[ 'subscriber' ];
-
-	// Loop and assign
-	foreach ( $caps as $cap => $grant ) {
-	  // Check to see if the user already has this capability, if so, don't re-add as that would override grant
-	  if ( !isset( $wp_roles->roles[ $role ][ 'capabilities' ][ $cap ] ) ) {
-	    $wp_roles->add_cap( $role, $cap, $grant );
-	  }
+	/**
+	 * Fired when the plugin is deactivated.
+	 *
+	 * @param boolean $network_wide True if WPMU superadmin uses
+	 *                              "Network Deactivate" action, false if
+	 *                              WPMU is disabled or plugin is
+	 *                              deactivated on an individual blog.
+	 *
+	 * @since {{plugin_version}}
+	 * 
+	 * @return void
+	 */
+	public static function deactivate( $network_wide ) {
+		if ( function_exists( 'is_multisite' ) && is_multisite() ) {
+			if ( $network_wide ) {
+				// Get all blog ids
+				$blogs = get_sites();
+				foreach ( $blogs as $blog ) {
+					switch_to_blog( $blog->blog_id );
+					self::single_deactivate();
+					restore_current_blog();
+				}
+				return;
+			}
+		}
+		self::single_deactivate();
 	}
-    }
-    //{{/unless}}
-    // Clear the permalinks
-    flush_rewrite_rules();
-  }
 
-  /**
-   * Fired for each blog when the plugin is deactivated.
-   *
-   * @since {{plugin_version}}
-   * @return void
-   */
-  private static function single_deactivate() {
-    // @TODO: Define deactivation functionality here
-    // Clear the permalinks
-    flush_rewrite_rules();
-  }
+	/**
+	 * Fired when a new site is activated with a WPMU environment.
+	 *
+	 * @param integer $blog_id ID of the new blog.
+	 *
+	 * @since {{plugin_version}}
+	 * 
+	 * @return void
+	 */
+	public function activate_new_site( $blog_id ) {
+		if ( 1 !== did_action( 'wpmu_new_blog' ) ) {
+			return;
+		}
+
+		switch_to_blog( $blog_id );
+		self::single_activate();
+		restore_current_blog();
+	}
+
+	/**
+	 * Fired for each blog when the plugin is activated.
+	 *
+	 * @since {{plugin_version}}
+	 * 
+	 * @return void
+	 */
+	private static function single_activate() {
+		$plugin = Plugin_Name::get_instance();
+		$plugin_slug = $plugin->get_plugin_slug();
+		//WPBPGen{{#unless system_capability-system}}
+		$plugin_roles = $plugin->get_plugin_roles();
+		//{{/unless}}
+		//WPBPGen{{#unless libraries_wpbp__requirements}}
+		// Requirements Detection System - read the doc/example in the library file
+		require_once( plugin_dir_path( __FILE__ ) . 'requirements.php' );
+		new Plugin_Requirements( PN_NAME, $plugin_slug, array(
+			'WP' => new WordPress_Requirement( '4.6.0' )
+				) );
+		//{{/unless}}
+		// @TODO: Define activation functionality here
+		//WPBPGen{{#unless system_capability-system}}
+		// add_role( 'advanced', __( 'Advanced' ) ); //Add a custom roles
+		global $wp_roles;
+		if ( !isset( $wp_roles ) ) {
+			$wp_roles = new WP_Roles;
+		}
+
+		foreach ( $wp_roles->role_names as $role => $label ) {
+			// If the role is a standard role, map the default caps, otherwise, map as a subscriber
+			$caps = ( array_key_exists( $role, $plugin_roles ) ) ? $plugin_roles[ $role ] : $plugin_roles[ 'subscriber' ];
+
+			// Loop and assign
+			foreach ( $caps as $cap => $grant ) {
+				// Check to see if the user already has this capability, if so, don't re-add as that would override grant
+				if ( !isset( $wp_roles->roles[ $role ][ 'capabilities' ][ $cap ] ) ) {
+					$wp_roles->add_cap( $role, $cap, $grant );
+				}
+			}
+		}
+		//{{/unless}}
+		// Clear the permalinks
+		flush_rewrite_rules();
+	}
+
+	/**
+	 * Fired for each blog when the plugin is deactivated.
+	 *
+	 * @since {{plugin_version}}
+	 * 
+	 * @return void
+	 */
+	private static function single_deactivate() {
+		// @TODO: Define deactivation functionality here
+		// Clear the permalinks
+		flush_rewrite_rules();
+	}
 
 }
 

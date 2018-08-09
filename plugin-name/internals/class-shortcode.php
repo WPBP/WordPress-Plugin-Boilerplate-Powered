48216929d2a98c155fba3fa6afb3afae28956035
Daniele Scasciafratte <mte90net@gmail.com>
2018-08-09 20:38:02 +0200

    add shortcode

---
+++ plugin-name/internals/class-shortcode.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Plugin_name
+ *
+ * @package   Plugin_name
+ * @author    {{author_name}} <{{author_email}}>
+ * @copyright {{author_copyright}}
+ * @license   {{author_license}}
+ * @link      {{author_url}}
+ */
+
+/**
+ * This class contain all the snippet or extra that improve the experience on the frontend
+ */
+class Pn_Shortcode extends Pn_Base {
+
+	/**
+	 * Initialize the snippet
+	 */
+	function initialize() {
+		parent::initialize();
+        add_shortcode( 'foobar', array( $this, 'foobar_func' ) );
+	}
+
+	/**
+	 * Shortcode example
+	 *
+	 * @param array $atts Parameters.
+	 *
+	 * @since {{plugin_version}}
+	 *
+	 * @return string
+	 */
+	public static function foobar_func( $atts ) {
+		extract( shortcode_atts( array(
+            'foo' => 'something',
+            'bar' => 'something else',
+        ), $atts ) );
+
+        return "foo = {$foo}";
+	}
+
+}

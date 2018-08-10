439f8f9887a9955200acb55578dd0dcdc61e2309
Daniele Scasciafratte <mte90net@gmail.com>
2018-08-10 17:48:44 +0200

    better json

--- plugin-name/tests/_support/extra.php
+++ plugin-name/tests/_support/extra.php
@@ -9,19 +9,19 @@ class WPH_Widget {
 	public $id_base;
 
 	function create_widget( $args ) {
-		
+
 	}
 
 	function form( $instance ) {
-		
+
 	}
-	
+
 	function update( $new_instance, $old_instance ) {
-		
+
 	}
 
 	function _register() {
-		
+
 	}
 
 }
--- plugin-name/wp-config-test.php
+++ plugin-name/wp-config-test.php
@@ -6,13 +6,13 @@
 define('DB_NAME', 'wordpress_unit_tests');
 
 /** MySQL database username */
-define('DB_USER', 'wp');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'wp');
+define('DB_PASSWORD', 'test');
 
 /** MySQL hostname */
-define('DB_HOST', '192.168.50.4');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');

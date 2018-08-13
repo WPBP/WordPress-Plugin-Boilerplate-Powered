a4b4a0505a5ae19245b077213f92920ea980216e
Daniele Scasciafratte <mte90net@gmail.com>
2018-08-13 18:23:33 +0200

    fix path

--- plugin-name/tests/_bootstrap.php
+++ plugin-name/tests/_bootstrap.php
@@ -2,7 +2,7 @@
 
 // This is global bootstrap for autoloading
 
-require_once __DIR__ . '/../composer/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 //WPBPGen{{#unless libraries_wpbp__widgets-helper}}
 include_once('_support/extra.php');
 //{{/unless}}

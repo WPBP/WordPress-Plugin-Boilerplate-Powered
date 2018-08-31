3d93359da5b7ca6eba89f374b7b641ac56181f91
Daniele Scasciafratte <mte90net@gmail.com>
2018-08-31 16:01:23 +0200

    working on other tests

--- plugin-name/tests/acceptance/AdminEnqueueCest.php
+++ plugin-name/tests/acceptance/AdminEnqueueCest.php
@@ -5,13 +5,22 @@ class AdminEnqueueCest {
 		$I->loginAsAdmin();
     }
 
-    function add_action_link(AcceptanceTester $I) {
+    function enqueue_admin_scripts(AcceptanceTester $I) {
 		$I->am('an administrator');
-		$I->wantTo('access to the plugin settings page and check the file enqueue');
+		$I->wantTo('access to the plugin settings page and check the scripts enqueue');
         $I->amOnPage('/wp-admin/admin.php?page=plugin-name');
-        $I->seeInPageSource('plugin-name-settings-styles-css');
-        $I->seeInPageSource('plugin-name-admin-styles-css');
-    }
+        $I->seeInPageSource('plugin-name-settings-script');
+		$I->seeInPageSource('plugin-name-admin-script');
+	}
+
+	function enqueue_admin_styles(AcceptanceTester $I) {
+		$I->am('an administrator');
+		$I->wantTo('access to the plugin settings page and check the style enqueue');
+		$I->amOnPage('/wp-admin/admin.php?page=plugin-name');
+		$I->seeInPageSource('plugin-name-settings-styles-css');
+		$I->seeInPageSource('plugin-name-admin-styles-css');
+	}
+
 
 }
 

3f28a194e6d0cd39b4eb1c505a938b779f028e88
Daniele Scasciafratte <mte90net@gmail.com>
2018-09-10 22:27:10 +0200

    added functional

--- plugin-name/tests/functional/CustomRestRouteCest.php
+++ plugin-name/tests/functional/CustomRestRouteCest.php
@@ -12,13 +12,19 @@ class CustomRestRouteCest {
 	 */
 	public function it_should_access_custom_endpoint( FunctionalTester $I ) {
 		$I->wantTo('access on the custom endpoint');
-		$I->sendAjaxGetRequest( '/wp-json/wp/v2/calc' );
-		codecept_debug($I->getResponseContent());
-
+		$I->sendGET( 'calc' );
 		$I->seeResponseCodeIs(HttpCode::OK); // 200
-		#$I->seeResponseMatchesJsonType([
-		#	'result' => '11',
-		#]);
+		$I->seeResponseContains('{"result":11}');
 	}
 
+	/**
+	 * @test
+	 * it should access custom endpoint with parameters
+	 */
+	public function it_should_access_custom_endpoint_with_parameters( FunctionalTester $I ) {
+		$I->wantTo('access on the custom endpoint');
+		$I->sendGET( 'calc', array( 'first' => 4, 'second' => 4 ) );
+		$I->seeResponseCodeIs(HttpCode::OK); // 200
+		$I->seeResponseContains('{"result":8}');
+	}
 }

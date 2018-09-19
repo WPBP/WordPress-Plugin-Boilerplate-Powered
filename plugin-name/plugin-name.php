6bfb41c30e161e67980229b24b4182f4ac244608
Daniele Scasciafratte <mte90net@gmail.com>
2018-09-19 11:29:38 +0200

    add block system for php version before 5.6

--- plugin-name/plugin-name.php
+++ plugin-name/plugin-name.php
@@ -7,6 +7,7 @@
  * A foundation off of which to build well-documented WordPress plugins that
  * also follow WordPress Coding Standards and PHP best practices.
 //{{/unless}}
+ *
  * @package   Plugin_Name
  * @author    {{author_name}} <{{author_email}}>
  * @copyright {{author_copyright}}
@@ -37,7 +38,7 @@ define( 'PN_NAME', '{{plugin_name}}' );
 define( 'PN_PLUGIN_ROOT', plugin_dir_path( __FILE__ ) );
 define( 'PN_PLUGIN_ABSOLUTE', __FILE__ );
 
-//WPBPGen{{#if language-files}}
+// WPBPGen{{#if language-files}}
 /**
  * Load the textdomain of the plugin
  *
@@ -49,14 +50,34 @@ function pn_load_plugin_textdomain() {
 }
 
 add_action( 'plugins_loaded', 'pn_load_plugin_textdomain', 1 );
-//{{/if}}
+// {{/if}}
+if ( ! version_compare( PHP_VERSION, '5.6.0', '>=' ) ) {
+	function pn_deactivate() {
+		deactivate_plugins( plugin_basename( __FILE__ ) );
+	}
+
+	function pn_show_deactivation_notice() {
+		echo wp_kses_post(
+			sprintf(
+				'<div class="notice notice-error"><p>%s</p></div>',
+				__( '"Plugin Name" requires PHP 5.6 or newer.', PN_TEXTDOMAIN )
+			)
+		);
+	}
+
+	add_action( 'admin_init', 'pn_deactivate' );
+	add_action( 'admin_notices', 'pn_show_deactivation_notice' );
+
+	// Return early to prevent loading the other includes.
+	return;
+}
 
 require_once( PN_PLUGIN_ROOT . 'vendor/autoload.php' );
 
 require_once( PN_PLUGIN_ROOT . 'internals/functions.php' );
 require_once( PN_PLUGIN_ROOT . 'internals/debug.php' );
 
-//WPBPGen{{#unless libraries_freemius__wordpress-sdk}}
+// WPBPGen{{#unless libraries_freemius__wordpress-sdk}}
 /**
  * Create a helper function for easy SDK access.
  *
@@ -64,24 +85,24 @@ require_once( PN_PLUGIN_ROOT . 'internals/debug.php' );
  * @return object
  */
 function pn_fs() {
-    global $pn_fs;
+	global $pn_fs;
 
-    if ( !isset( $pn_fs ) ) {
+	if ( !isset( $pn_fs ) ) {
 		require_once( PN_PLUGIN_ROOT . 'vendor/freemius/wordpress-sdk/start.php' );
 		$pn_fs = fs_dynamic_init(
 			array(
-            'id'             => '',
-			'slug'           => 'plugin-name',
-			'public_key'     => '',
-			'is_live'        => false,
-			'is_premium'     => true,
-			'has_addons'     => false,
-			'has_paid_plans' => true,
-			'menu'           => array(
-				'slug' => 'plugin-name',
-			),
-		)
-	);
+				'id'             => '',
+				'slug'           => 'plugin-name',
+				'public_key'     => '',
+				'is_live'        => false,
+				'is_premium'     => true,
+				'has_addons'     => false,
+				'has_paid_plans' => true,
+				'menu'           => array(
+					'slug' => 'plugin-name',
+				),
+			)
+		);
 	}
 
 	return $pn_fs;
@@ -89,8 +110,7 @@ function pn_fs() {
 
 // Init Freemius.
 // pn_fs();
-//{{/unless}}
-
+// {{/unless}}
 if ( ! wp_installing() ) {
 	add_action( 'plugins_loaded', array( 'Pn_Initialize', 'get_instance' ) );
 }

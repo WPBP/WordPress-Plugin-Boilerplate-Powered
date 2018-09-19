9fb7b37c42f52f6667f77a7acb3148cbaa3a1dd5
Daniele Scasciafratte <mte90net@gmail.com>
2018-09-19 14:39:44 +0200

    cli test working

--- plugin-name/tests/functional/CliCest.php
+++ plugin-name/tests/functional/CliCest.php
@@ -1,20 +1,13 @@
 <?php
 
 class CliCest {
-	/**
-	 * @return WPCLI
-	 */
-	private function make_instance() {
-		return new WPCLI($this->moduleContainer->reveal(), $this->config, $this->executor->reveal());
-	}
 
 	/**
 	 * It should not fail!
 	 *
 	 * @test
 	 */
-	public function it_should_not_fail_when_default_command_is_executed() {
-		$sut = $this->make_instance();
-		$sut->cli('pn_commandname');
+	public function it_should_not_fail_when_default_command_is_executed(FunctionalTester $I) {
+		$I->cli('pn_commandname');
 	}
 }

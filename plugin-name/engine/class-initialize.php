b868b86b10c8bc738e3c6105a3af599b21170bd1
Daniele Scasciafratte <mte90net@gmail.com>
2019-05-07 16:49:13 +0200

    added new methods based on https://github.com/szepeviktor/debian-server-tools/blob/master/webserver/wordpress/_core-is.php

--- plugin-name/engine/class-initialize.php
+++ plugin-name/engine/class-initialize.php
@@ -55,95 +55,98 @@ class Pn_Initialize {
         // WPBPGen{{#unless libraries_wpbp__widgets-helper}}
         $this->classes[] = 'Pn_Widgets';
         // {{/unless}}
-        // WPBPGen{{#unless system_rest}}
-        $this->classes[] = 'Pn_Rest';
-        // {{/unless}}
-        // WPBPGen{{#unless system_transient}}
-        $this->classes[] = 'Pn_Transient';
-        // {{/unless}}
+		// WPBPGen{{#unless system_rest}}
+		if ( $this->is->request( 'rest' ) ) {
+			$this->classes[] = 'Pn_Rest';
+		}
+		// {{/unless}}
 
-        // WPBPGen{{#unless wpcli}}
-        if ( $this->is->request( 'cli' ) ) {
-            $this->classes[] = 'Pn_Cli';
-        }
-        // {{/unless}}
+		// WPBPGen{{#unless system_transient}}
+		$this->classes[] = 'Pn_Transient';
+		// {{/unless}}
 
-        // WPBPGen{{#unless ajax_public}}
-        if ( $this->is->request( 'ajax' ) ) {
-            $this->classes[] = 'Pn_Ajax';
-        }
-        // {{/unless}}
+		// WPBPGen{{#unless wpcli}}
+		if ( $this->is->request( 'cli' ) ) {
+			$this->classes[] = 'Pn_Cli';
+		}
+		// {{/unless}}
 
-        if ( $this->is->request( 'admin' ) ) {
+		// WPBPGen{{#unless ajax_public}}
+		if ( $this->is->request( 'ajax' ) ) {
+			$this->classes[] = 'Pn_Ajax';
+		}
+		// {{/unless}}
+
+		if ( $this->is->request( 'admin_backend' ) ) {
 			// WPBPGen{{#unless ajax_public}}
 			if ( $this->is->request( 'ajax' ) ) {
 				$this->classes[] = 'Pn_Ajax_Admin';
 			}
 			// {{/unless}}
 			// WPBPGen{{#unless libraries_wpbp__pointerplus}}
-            $this->classes[] = 'Pn_Pointers';
+			$this->classes[] = 'Pn_Pointers';
 			// {{/unless}}
 			// WPBPGen{{#unless libraries_mte90__wp-contextual-help}}
-            $this->classes[] = 'Pn_ContextualHelp';
+			$this->classes[] = 'Pn_ContextualHelp';
 			// {{/unless}}
 			// WPBPGen{{#unless act-deact_actdeact}}
-            $this->classes[] = 'Pn_Admin_ActDeact';
+			$this->classes[] = 'Pn_Admin_ActDeact';
 			// {{/unless}}
 			// WPBPGen{{#unless libraries_nathanielks__wp-admin-notice}}
-            $this->classes[] = 'Pn_Admin_Notices';
+			$this->classes[] = 'Pn_Admin_Notices';
 			// {{/unless}}
 			// WPBPGen{{#unless admin-assets_admin-page}}
-            $this->classes[] = 'Pn_Admin_Settings_Page';
+			$this->classes[] = 'Pn_Admin_Settings_Page';
 			// {{/unless}}
 			// WPBPGen{{#unless admin-assets_admin-js && admin-assets_admin-css}}
-            $this->classes[] = 'Pn_Admin_Enqueue';
+			$this->classes[] = 'Pn_Admin_Enqueue';
 			// {{/unless}}
 			// WPBPGen{{#unless backend_impexp-settings}}
-            $this->classes[] = 'Pn_Admin_ImpExp';
+			$this->classes[] = 'Pn_Admin_ImpExp';
 			// {{/unless}}
-        }
+		}
 
-        if ( $this->is->request( 'frontend' ) ) {
+		if ( $this->is->request( 'frontend' ) ) {
 			// WPBPGen{{#unless public-assets_js && public-assets_css && frontend_wp-localize-script}}
-            $this->classes[] = 'Pn_Enqueue';
-            // {{/unless}}
-            $this->classes[] = 'Pn_Extras';
-        }
+			$this->classes[] = 'Pn_Enqueue';
+			// {{/unless}}
+			$this->classes[] = 'Pn_Extras';
+		}
 
-        $this->classes = apply_filters( 'pn_class_instances', $this->classes );
+		$this->classes = apply_filters( 'pn_class_instances', $this->classes );
 
-        return $this->load_classes();
-    }
+		return $this->load_classes();
+	}
 
-    private function load_classes() {
-        foreach ( $this->classes as &$class ) {
-            $class = apply_filters( strtolower( $class ) . '_instance', $class );
-            $temp  = new $class;
-            $temp->initialize();
-        }
-    }
+	private function load_classes() {
+		foreach ( $this->classes as &$class ) {
+			$class = apply_filters( strtolower( $class ) . '_instance', $class );
+			$temp  = new $class;
+			$temp->initialize();
+		}
+	}
 
-    /**
-     * Return an instance of this class.
-     *
-     * @since {{plugin_version}}
-     *
-     * @return object A single instance of this class.
-     */
-    public static function get_instance() {
-        // If the single instance hasn't been set, set it now.
-        if ( null === self::$instance ) {
-            try {
-                self::$instance = new self;
-            } catch ( Exception $err ) {
-                do_action( 'plugin_name_initialize_failed', $err );
-                if ( WP_DEBUG ) {
-                    throw $err->getMessage();
-                }
-            }
-        }
+	/**
+	 * Return an instance of this class.
+	 *
+	 * @since {{plugin_version}}
+	 *
+	 * @return object A single instance of this class.
+	 */
+	public static function get_instance() {
+		// If the single instance hasn't been set, set it now.
+		if ( null === self::$instance ) {
+			try {
+				self::$instance = new self;
+			} catch ( Exception $err ) {
+				do_action( 'plugin_name_initialize_failed', $err );
+				if ( WP_DEBUG ) {
+					throw $err->getMessage();
+				}
+			}
+		}
 
-        return self::$instance;
-    }
+		return self::$instance;
+	}
 
 }
--- plugin-name/engine/class-is-methods.php
+++ plugin-name/engine/class-is-methods.php
@@ -16,6 +16,24 @@
 class Pn_Is_Methods {
 
 	/**
+	 * Whether given user is an administrator.
+	 *
+	 * @param \WP_User $user The given user.
+	 * @return bool
+	 */
+	public static function is_user_admin( $user = '' ) {
+		if ( empty( $user ) ) {
+			$user = wp_get_current_user();
+		}
+
+		if ( ! is_object( $user ) || ! $user instanceof WP_User ) {
+			_doing_it_wrong( __METHOD__, 'To check if the user is admin is required a WP_User object.', '1.0.0' );
+		}
+
+		return is_multisite() ? user_can( $user, 'manage_network' ) : user_can( $user, 'manage_options' );
+	}
+
+	/**
 	 * What type of request is this?
 	 *
 	 * @since {{plugin_version}}
@@ -25,17 +43,33 @@ class Pn_Is_Methods {
 	 */
 	public function request( $type ) {
 		switch ( $type ) {
-			case 'admin':
-				return $this->is_admin();
+			case 'admin_backend':
+				return $this->is_admin_backend();
 			case 'ajax':
 				return $this->is_ajax();
+			case 'installing_wp':
+				return $this->is_installing_wp();
+			case 'rest':
+				return $this->is_rest();
 			case 'cron':
 				return $this->is_cron();
 			case 'frontend':
 				return $this->is_frontend();
 			case 'cli':
 				return $this->is_cli();
+			default:
+				_doing_it_wrong( __METHOD__, esc_html( sprintf( 'Unknown request type: %s', $type ) ), '1.0.0' );
+				return false;
 		}
+	}
+
+	/**
+	 * Is installing WP
+	 *
+	 * @return boolean
+	 */
+	public function is_installing_wp() {
+		return defined( 'WP_INSTALLING' );
 	}
 
 	/**
@@ -43,7 +77,7 @@ class Pn_Is_Methods {
 	 *
 	 * @return boolean
 	 */
-	public function is_admin() {
+	public function is_admin_backend() {
 		return is_user_logged_in() && is_admin();
 	}
 
@@ -54,6 +88,15 @@ class Pn_Is_Methods {
 	 */
 	public function is_ajax() {
 		return ( function_exists( 'wp_doing_ajax' ) && wp_doing_ajax() ) || defined( 'DOING_AJAX' );
+	}
+
+	/**
+	 * Is rest
+	 *
+	 * @return boolean
+	 */
+	public function is_rest() {
+		return defined( 'REST_REQUEST' );
 	}
 
 	/**
@@ -71,7 +114,7 @@ class Pn_Is_Methods {
 	 * @return boolean
 	 */
 	public function is_frontend() {
-		return ( ! is_admin() || !$this->is_ajax() ) && !$this->is_cron() && ! defined( 'REST_REQUEST' );
+		return ( ! $this->is_admin_backend() || ! $this->is_ajax() ) && ! $this->is_cron() && ! $this->is_rest() && ! is_robots() && ! is_feed() && ! is_trackback();
 	}
 
 	/**

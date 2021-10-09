bbb7a65ecb304dda472c52ebaa971d05a511fe23
jlzaratec <jlzaratec@hotmail.com>
2021-10-09 15:18:43 -0500

    Move to GPLv3

--- plugin-name/templates/content-demo.php
+++ plugin-name/templates/content-demo.php
@@ -1,3 +1,22 @@
+<?php
+/*
+* This file is part of WordPress Plugin Boilerplate Powered.
+*
+* WordPress Plugin Boilerplate Powered is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* WordPress Plugin Boilerplate Powered is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+*/
+?>
+
 <!-- @TODO: Provide other markup for your page here. -->
 <div>
 	<?php

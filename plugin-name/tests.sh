1c64124e78ad5634ac1ab77d424f8b985b34b0e7
jlzaratec <jlzaratec@hotmail.com>
2021-10-11 13:36:28 -0500

    Add license GPLv3 to files README.txt, LICENSE, LICENSE.TXT, composer.json and README.md

--- plugin-name/tests.sh
+++ plugin-name/tests.sh
@@ -1,23 +1,5 @@
 #!/bin/sh
 
-#
-# This file is part of WordPress Plugin Boilerplate Powered.
-#
-# WordPress Plugin Boilerplate Powered is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# WordPress Plugin Boilerplate Powered is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
-
-# Do you want to execute a specific test suites? this script accept as first paramete the suite name!
-
 if ! lsof -Pi :4444 -sTCP:LISTEN -t >/dev/null; then
 	echo " - Run Selenium"
 	java -jar -Dwebdriver.chrome.driver=/opt/selenium/chromedriver /opt/selenium/selenium-server.jar 2> /dev/null &
--- plugin-name/tests/custom.sh
+++ plugin-name/tests/custom.sh
@@ -1,23 +1,5 @@
 #!/bin/sh
 
-# This file is part of WordPress Plugin Boilerplate Powered.
-#
-# WordPress Plugin Boilerplate Powered is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# WordPress Plugin Boilerplate Powered is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
-
-# You can add there your script to execeute before codeception
-# Can be useful for check if MySql is running as example
-
 UP=$(sudo service mysql status | grep -c "stopped\|dead\|failed");
 if [ "$UP" -eq 2 ]; then
 	echo " - Mysql is not running, launched right now"

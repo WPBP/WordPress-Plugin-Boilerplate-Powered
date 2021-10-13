1c334e4d1fc7d0d37d9adee4aab1e13c0bf807ef
Daniele Scasciafratte <mte90net@gmail.com>
2021-10-13 19:21:14 +0200

    revert change in tests.sh

--- plugin-name/tests.sh
+++ plugin-name/tests.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# Do you want to execute a specific test suites? this script accept as first paramete the suite name!
+
 if ! lsof -Pi :4444 -sTCP:LISTEN -t >/dev/null; then
 	echo " - Run Selenium"
 	java -jar -Dwebdriver.chrome.driver=/opt/selenium/chromedriver /opt/selenium/selenium-server.jar 2> /dev/null &

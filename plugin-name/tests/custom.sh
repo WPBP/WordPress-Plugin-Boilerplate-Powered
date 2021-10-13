f6bc59dcf7b085e66db4c9a2d3e4a13c9b480a30
Daniele Scasciafratte <mte90net@gmail.com>
2021-10-13 19:20:39 +0200

    revert change in custom.sh

--- plugin-name/tests/custom.sh
+++ plugin-name/tests/custom.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# You can add there your script to execute before codeception
+# Can be useful for check if MySql is running as example
+
 UP=$(sudo service mysql status | grep -c "stopped\|dead\|failed");
 if [ "$UP" -eq 2 ]; then
 	echo " - Mysql is not running, launched right now"

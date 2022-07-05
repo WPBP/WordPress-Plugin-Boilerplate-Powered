1c80607e9177c1f1db1d22edece498531434c473
Daniele Scasciafratte <mte90net@gmail.com>
2022-07-05 11:06:34 +0200

    fix(lint): tiny

--- plugin-name/assets/src/plugin-public.js
+++ plugin-name/assets/src/plugin-public.js
@@ -5,7 +5,7 @@ window.onload = () => {
 	// Write in console log the PHP value passed in enqueue_js_vars in frontend/Enqueue.php
 	( () => {
 		// WPBPGen{{#if system_rest}}
-		jQuery('#example-demo-button').on('click', function() {
+		jQuery("#example-demo-button").on("click", function() {
 			jQuery.ajax({
 				method: "POST",
 				url: window.location + "wp-json/wp/v2/demo/example",
@@ -13,7 +13,7 @@ window.onload = () => {
 					nonce: window.example_demo.nonce
 				},
 				beforeSend: function (xhr) {
-					xhr.setRequestHeader('X-WP-Nonce', window.example_demo.wp_rest);
+					xhr.setRequestHeader("X-WP-Nonce", window.example_demo.wp_rest);
 				}
 			})
 			.done(function( msg ) {

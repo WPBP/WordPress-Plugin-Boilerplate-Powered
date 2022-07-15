bf465cc8814b169111e0c0f767e17afb0eda647e
Daniele Scasciafratte <mte90net@gmail.com>
2022-07-15 19:13:43 +0200

    feat(tests): various fixes

--- plugin-name/tests.sh
+++ plugin-name/tests.sh
@@ -3,7 +3,10 @@
 # Do you want to execute a specific test suites? this script accept as first paramete the suite name!
 
 if ! lsof -Pi :4444 -sTCP:LISTEN -t >/dev/null; then
-	echo " - Run Selenium"
+	echo " - Run Chromedriver"
+	# wget https://chromedriver.storage.googleapis.com/103.0.5060.53/chromedriver_linux64.zip > /dev/null 2>&1
+	# unzip chromedriver_linux64.zip
+    # nohup ./chromedriver --port=4444 &
 	java -jar -Dwebdriver.chrome.driver=/opt/selenium/chromedriver /opt/selenium/selenium-server.jar 2> /dev/null &
 fi
 

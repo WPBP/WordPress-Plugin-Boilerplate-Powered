9897addfd93458c0608406bb202e772dd90da93b
Daniele Scasciafratte <mte90net@gmail.com>
2022-07-15 19:46:49 +0200

    feat(tests): various fixes

--- plugin-name/tests.sh
+++ plugin-name/tests.sh
@@ -4,10 +4,11 @@
 
 if ! lsof -Pi :4444 -sTCP:LISTEN -t >/dev/null; then
 	echo " - Run Chromedriver"
-	# wget https://chromedriver.storage.googleapis.com/103.0.5060.53/chromedriver_linux64.zip > /dev/null 2>&1
-	# unzip chromedriver_linux64.zip
-    # nohup ./chromedriver --port=4444 &
-	java -jar -Dwebdriver.chrome.driver=/opt/selenium/chromedriver /opt/selenium/selenium-server.jar 2> /dev/null &
+	if [ ! -f "/tmp/chromedriver" ]; then
+		wget https://chromedriver.storage.googleapis.com/103.0.5060.53/chromedriver_linux64.zip > /dev/null 2>&1
+		unzip chromedriver_linux64.zip
+	fi
+    ./chromedriver --port=4444 &
 fi
 
 echo " - Run Codeception"

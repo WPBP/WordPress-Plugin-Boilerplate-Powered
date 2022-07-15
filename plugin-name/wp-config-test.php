daeb8b59637f7c4df6c79848f15f439f48ab34f8
Daniele Scasciafratte <mte90net@gmail.com>
2022-07-15 11:44:31 +0200

    ci(wp): install for test

--- plugin-name/wp-config-test.php
+++ plugin-name/wp-config-test.php
@@ -11,7 +11,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', 'test');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost:3306');
+define('DB_HOST', '127.0.0.1:3306');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
